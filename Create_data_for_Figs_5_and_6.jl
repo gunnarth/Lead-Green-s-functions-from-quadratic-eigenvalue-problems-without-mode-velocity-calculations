5e5c9773719abe4bb4d67434de3e41ec581e8b07
Gunnar Thorgilsson <gunnarth@ru.is>
2022-11-13 17:33:52 +0000

    Minor fixes and cleanup

--- Create_data_for_Figs_5_and_6.jl
+++ Create_data_for_Figs_5_and_6.jl
@@ -27,22 +27,22 @@ include(joinpath(DIR, "src", "selfenergy_functions",  "LopesSanchos.jl"))
 include(joinpath(DIR, "src", "helper_functions",      "Timer.jl"))
 
 #=======Setting Parameters=====================================#
-etaList=[10.0^(-p) for p in [16,]]#,15,14,13]] #Imaginary shift to energy
+etaList=[10.0^(-p) for p in [16,15,14,13]] #Imaginary shift to energy
 
 #--list of energy points to calculate at
-Energies=collect(0:1.0:10) #Rashba
-Energies_ribbon=collect(-5:1.0:5) #Nanoribbon
+Energies=collect(0:0.05:10) #Rashba
+Energies_ribbon=collect(-5:0.05:5) #Nanoribbon
 
 
 repIt=10 #how many time to repeat calculation
-N=12     #size parameter for matrices, corresponds to size 50
+N=24     #size parameter for matrices, corresponds to size 98
 
 #--Hamiltonian matrices calculated and put in a dictionary
 Hr,Vr=rashbaMatrices((2*N+1),1);
 Hg,Vg=grapheneMatrices(N);
 
 SystemType=Dict("Nanoribbon" => (Hg,Vg), "Rashba" => (Hr,Vr))
-#GESstandard=GESpower2
+
 #========Dataframe for results created==========================#
 dfE=DataFrame(E = Float64[],
               S = String[],
@@ -57,8 +57,8 @@ dfE=DataFrame(E = Float64[],
 #=======Iteration over system, eta and energy===================#
 for key in keys(SystemType)
     for eta in etaList
-        println(eta)
         for (i,e) in enumerate(Energies)
+
             if key=="Nanoribbon"
                 e=Energies_ribbon[i]
             end
@@ -79,5 +79,5 @@ for key in keys(SystemType)
                     
         end
     end
-    CSV.write(joinpath(DIR,"Data","TimingData","Data_for_fig_5_and_6.csv"), dfE)
+    CSV.write(joinpath(DIR,"Data","TimingData","Data_for_figs_5_and_6.csv"), dfE)
 end
--- src/helper_functions/CountBands.jl
+++ src/helper_functions/CountBands.jl
@@ -5,7 +5,6 @@ function CountBands(E,H,V,etaR=1e-14)
     n=convert(Int,size(H)[1])
     O=zeros(ComplexF64,n,n)
     I0=Matrix{ComplexF64}(I, n, n) 
-    #C=(-(E+5.0e-16)*I0+H)
     C=(-(E+etaR)*I0+H)
 
     K=V'
@@ -19,7 +18,6 @@ function CountBands(E,H,V,etaR=1e-14)
 
     #---Generlized Schur factoring performed-------#
     F=schur(A,B)
-    #eigenvalues=F[:alpha]./F[:beta]
     eigenvalues=F.α./F.β 
     de=1.0e-10
     if any(abs.(norm.(eigenvalues).-1.0).<de)
